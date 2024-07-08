58db80b95ae72b1546304ad3f334eb1383edb1bf
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2024-07-08 20:01:36 +0100

    fix: return 403 status code for token related error

--- middleware/auth/auth.js
+++ middleware/auth/auth.js
@@ -8,7 +8,7 @@ function verifyToken(accessToken, secret) {
     return new Promise((resolve, reject) => {
         jwt.verify(accessToken, secret, (err, decoded) => {
             if (err) {
-                return reject(new CustomError('Access Denied! Invalid token', 403));
+                return reject(new CustomError('Access Denied! Invalid token', 401));
             }
             resolve(decoded);
         });
@@ -19,11 +19,11 @@ function verifyToken(accessToken, secret) {
 const verify_jwt = async (req, res, next) => {
     try {
         if (!req.headers.authorization) {
-            throw new CustomError('Acess Denied!, No auth header', 403);
+            throw new CustomError('Acess Denied!, No auth header', 401);
         }
         const accessToken = req.headers.authorization.split(' ')[1].replace(/ /g, '');
         if (!accessToken) {
-            throw new CustomError('Acess Denied!, No access token', 403);
+            throw new CustomError('Acess Denied!, No access token', 401);
         }
         // To do: Check id access token is blocked -> Logged out user token
         const decoded = await verifyToken(accessToken, process.env.JWT_ACCESS_SECRET);
