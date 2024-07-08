0ea2c0d0baf59858a4109f6fb33a8619468e2564
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2024-07-08 20:03:43 +0100

    fix: return 403 status code for token related error

--- models/index.js
+++ models/index.js
@@ -4,26 +4,26 @@ const Organisation = require('./organization.model');
 const UserOrganisations = require('./userorganisations.model');
 
 // Create a new sequelize instance
-const sequelize =  new Sequelize({
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    logging: false
-})
+// const sequelize =  new Sequelize({
+//     dialect: 'postgres',
+//     host: process.env.DB_HOST,
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     logging: false
+// })
 
-// const sequelize = new Sequelize(process.env.DB_URL, {
-//   dialect: 'postgres',
-//   protocol: 'postgres',
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     }
-//   },
-//   logging: false
-// });
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  },
+  logging: false
+});
 
 // db object that holds db related properties
 const db = {};
